0e5e33963bce84e5013a30cd25f3020cc6ec18eb
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-06-05 15:47:07 +0530

    Added models for AuthCookie and AuthCredential

---
+++ src/models/AuthCookie.ts
@@ -0,0 +1,35 @@
+// TYPES
+import { IAuthCookie } from "../types/AuthCookie";
+
+/**
+ * The cookie containing the tokens that are used to authenticate against Twitter.
+ *
+ * @public
+ */
+export class AuthCookie implements IAuthCookie {
+    /* eslint-disable */
+    /** Token used to authenticate a device. */
+    kdt: string;
+
+    /** Token used to authenticate a user using a Twitter ID. */
+    twid: string;
+
+    /** The CSRF token of the session. */
+    ct0: string;
+
+    /** The authentication token used while logging in to the account. */
+    auth_token: string;
+    /* eslint-enable */
+
+    /**
+     * Creates a new AuthCookie object from the given cookie string.
+     * 
+     * @param cookie The cookie string containing all the required cookies.
+     */
+    constructor(cookie: string) {
+        this.auth_token = '';
+        this.ct0 = '';
+        this.kdt = '';
+        this.twid = '';
+    }
+}
---
+++ src/models/AuthCredential.ts
@@ -0,0 +1,33 @@
+// TYPES
+import { IAuthCredential } from "../types/AuthCredential";
+
+// MODELS
+import { AuthCookie } from './AuthCookie';
+
+/**
+ * The credentials for authenticating against Twitter.
+ *
+ * Depending on which tokens are present, the authentication type is determined as follows:
+ * - authToken, guestToken => Guest authentication.
+ * - authToken, csrfToken, cookie => User authentication.
+ * - authToken, guestToken, cookie => Guest authentication while logging in.
+ *
+ * @public
+ */
+export class AuthCredential implements IAuthCredential {
+    /** The bearer token from twitter.com. */
+    authToken?: string;
+
+    /** The guest token provided by Twitter API. */
+    guestToken?: string;
+
+    /** The guest token. */
+    csrfToken?: string;
+
+    /** The cookie of the twitter account, which is used to authenticate against twitter. */
+    cookie?: string;
+
+    async get(cookie: AuthCookie): Promise<AuthCredential> {
+
+    }
+}
