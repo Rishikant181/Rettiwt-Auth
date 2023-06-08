ad324f71d7cfa07bf7813de181f91c9ac9f1cd44
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-06-08 16:15:49 +0530

    Added account credential

---
+++ src/models/AccountCredential.ts
@@ -0,0 +1,27 @@
+// TYPES
+import { IAccountCredential } from "../types/AccountCredential";
+
+/**
+ * The credentials of the Twitter account to be logged into.
+ */
+export class AccountCredential implements IAccountCredential {
+    /** The mail to the Twitter account. */
+    email: string;
+
+    /** The username associated with the Twitter account. */
+    userName: string;
+
+    /** The password to the Twitter account. */
+    password: string;
+
+    /**
+     * Initializes a new AccountCredential instance.
+     * 
+     * @param cred The credentials to the Twitter account.
+     */
+    constructor(cred: AccountCredential) {
+        this.email = cred.email;
+        this.userName = cred.userName;
+        this.password = cred.password;
+    }
+}
---
+++ src/types/AccountCredential.ts
@@ -0,0 +1,13 @@
+/**
+ * The credentials of the Twitter account to be logged into.
+ */
+export interface IAccountCredential {
+    /** The mail to the Twitter account. */
+    email: string;
+
+    /** The username associated with the Twitter account. */
+    userName: string;
+
+    /** The password to the Twitter account. */
+    password: string;
+}
