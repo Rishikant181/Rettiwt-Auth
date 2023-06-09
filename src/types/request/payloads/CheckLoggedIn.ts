0981a4e485afdab3ef430fd9e4971d155404347a
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-06-09 16:26:50 +0530

    Divided the login payloads

---
+++ src/types/request/payloads/CheckLoggedIn.ts
@@ -0,0 +1,3 @@
+export interface ICheckLoggedInInput {
+    link: string
+}
--- src/types/request/payloads/LoginSubtask.ts
+++ src/types/request/payloads/LoginSubtask.ts
@@ -1,3 +1,10 @@
+// TYPES
+import { IJsInstrumentationInput } from './JsInstrumentation';
+import { IUserIdentifierInput } from './UserIdentifier';
+import { IAlternateUserIdentifierInput } from './AlternateUserIdentifier';
+import { IPasswordInput } from './Password';
+import { ICheckLoggedInInput } from './CheckLoggedIn';
+
 export interface ILoginSubtaskPayload {
     flow_token: string
     subtask_inputs: ILoginSubtaskInput[]
@@ -10,41 +17,4 @@ export interface ILoginSubtaskInput {
     enter_text?: IAlternateUserIdentifierInput
     enter_password?: IPasswordInput
     check_logged_in_account?: ICheckLoggedInInput
-}
-
-export interface IJsInstrumentationInput {
-    response: string
-    link: string
-}
-
-export interface IUserIdentifierInput {
-    setting_responses: ISettingResponse[]
-    link: string
-}
-
-export interface ISettingResponse {
-    key: string
-    response_data: IResponseData
-}
-
-export interface IResponseData {
-    text_data: ITextData
-}
-
-export interface ITextData {
-    result: string
-}
-
-export interface IAlternateUserIdentifierInput {
-    text: string
-    link: string
-}
-
-export interface IPasswordInput {
-    password: string
-    link: string
-}
-
-export interface ICheckLoggedInInput {
-    link: string
 }
