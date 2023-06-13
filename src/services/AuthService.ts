2d3abe58e051311a6f6cf83d22566ef9759135f8
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-06-13 19:03:18 +0530

    Added docs

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,23 +11,28 @@ import { AuthCredential } from '../models/AuthCredential';
  * A class that deals with authenticating against Twitter API.
  */
 export class AuthService {
-	async getGuestCredential(): Promise<AuthCredential> {
-		// Creating a new blank credential
-		const cred: AuthCredential = new AuthCredential();
+    /**
+     * Fetches a guest token, for guest authentication, from Twitter API.
+     * 
+     * @returns The credentials containing the guest token.
+     */
+    async getGuestCredential(): Promise<AuthCredential> {
+        // Creating a new blank credential
+        const cred: AuthCredential = new AuthCredential();
 
-		// Getting the guest token
-		await axios
-			.post<IGuestTokenResponse>('https://api.twitter.com/1.1/guest/activate.json', null, {
-				headers: {
-					/* eslint-disable */
-					Authorization: `Bearer ${cred.authToken as string}`,
-					/* eslint-enable */
-				},
-			})
-			.then((res) => {
-				cred.guestToken = res.data.guest_token;
-			});
+        // Getting the guest token
+        await axios
+            .post<IGuestTokenResponse>('https://api.twitter.com/1.1/guest/activate.json', null, {
+                headers: {
+                    /* eslint-disable */
+                    Authorization: `Bearer ${cred.authToken as string}`,
+                    /* eslint-enable */
+                },
+            })
+            .then((res) => {
+                cred.guestToken = res.data.guest_token;
+            });
 
-		return cred;
-	}
+        return cred;
+    }
 }
