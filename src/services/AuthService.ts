eb52b51b2bb93c0d8dc4f4dba7c163219266b0b2
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-06-13 18:46:20 +0530

    Formatted and linted code

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,19 +11,23 @@ import { AuthCredential } from '../models/AuthCredential';
  * A class that deals with authenticating against Twitter API.
  */
 export class AuthService {
-    async getGuestCredential(): Promise<AuthCredential> {
-        // Creating a new blank credential
-        const cred: AuthCredential = new AuthCredential();
+	async getGuestCredential(): Promise<AuthCredential> {
+		// Creating a new blank credential
+		const cred: AuthCredential = new AuthCredential();
 
-        // Getting the guest token
-        await axios.post<IGuestTokenResponse>('https://api.twitter.com/1.1/guest/activate.json', null, {
-            headers: {
-                'Authorization': `Bearer ${cred.authToken as string}`
-            }
-        }).then(res => {
-            cred.guestToken = res.data.guest_token;
-        });
+		// Getting the guest token
+		await axios
+			.post<IGuestTokenResponse>('https://api.twitter.com/1.1/guest/activate.json', null, {
+				headers: {
+					/* eslint-disable */
+					Authorization: `Bearer ${cred.authToken as string}`,
+					/* eslint-enable */
+				},
+			})
+			.then((res) => {
+				cred.guestToken = res.data.guest_token;
+			});
 
-        return cred;
-    }
+		return cred;
+	}
 }
