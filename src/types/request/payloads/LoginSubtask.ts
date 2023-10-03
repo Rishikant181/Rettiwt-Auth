a6b5bd458e6e373eb3fe24474c0af912cec0b9b9
Rishikant Sahu <rishikantsahu181@gmail.com>
2023-10-04 00:56:17 +0530

    Fixed linter errors

--- src/types/request/payloads/LoginSubtask.ts
+++ src/types/request/payloads/LoginSubtask.ts
@@ -6,15 +6,19 @@ import { IPasswordInput } from './subtasks/Password';
 import { IAccountDuplicationCheck } from './subtasks/AccountDuplicationCheck';
 
 export interface ILoginSubtaskPayload {
+	/* eslint-disable @typescript-eslint/naming-convention */
 	flow_token: string;
 	subtask_inputs: ILoginSubtaskInput[];
+	/* eslint-enable @typescript-eslint/naming-convention */
 }
 
 export interface ILoginSubtaskInput {
+	/* eslint-disable @typescript-eslint/naming-convention */
 	subtask_id: string;
 	js_instrumentation?: IJsInstrumentationInput;
 	settings_list?: IUserIdentifierInput;
 	enter_text?: IAlternateUserIdentifierInput;
 	enter_password?: IPasswordInput;
 	check_logged_in_account?: IAccountDuplicationCheck;
+	/* eslint-enable @typescript-eslint/naming-convention */
 }
--- src/types/request/payloads/subtasks/UserIdentifier.ts
+++ src/types/request/payloads/subtasks/UserIdentifier.ts
@@ -1,15 +1,21 @@
 export interface IUserIdentifierInput {
+	/* eslint-disable @typescript-eslint/naming-convention */
 	setting_responses: ISettingResponse[];
 	link: string;
+	/* eslint-enable @typescript-eslint/naming-convention */
 }
 
 export interface ISettingResponse {
+	/* eslint-disable @typescript-eslint/naming-convention */
 	key: string;
 	response_data: IResponseData;
+	/* eslint-enable @typescript-eslint/naming-convention */
 }
 
 export interface IResponseData {
+	/* eslint-disable @typescript-eslint/naming-convention */
 	text_data: ITextData;
+	/* eslint-enable @typescript-eslint/naming-convention */
 }
 
 export interface ITextData {
